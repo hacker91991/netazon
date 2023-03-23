86bbf3be33443ffbbbe07c67558cc5c398d0c35a
pranav <pranavpc@gmail.com>
2023-03-23 17:08:08 +0530

    complete files

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+export { MovieList }  from "./MovieList";
+export { MovieDetail }  from "./MovieDetail";
+export { PageNotFound }  from "./PageNotFound";
+export { Search }  from "./Search";
+
+
+
+
+
+
+
+
+
+
